17b8b176a844102ee4e68dc5c155ccae5fed1f63
harshasanjeeva <harshasanjiva@gmail.com>
2018-01-13 19:48:12 -0800

    production

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
 
-module.exports = {
+
+var config = {
   entry: './app/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -23,3 +25,15 @@ module.exports = {
     })
   ]
 };
+
+if(process.env.NODE_ENV === 'production'){
+  config.plugins.push(
+   new webpack.DefinePlugin({
+     'process.env' : {
+        'NODE_ENV': JSON.stringify('production') 
+     }
+   }),
+   new webpack.optimize.UglifyJsPlugin()
+  )
+}
+module.exports = config;
